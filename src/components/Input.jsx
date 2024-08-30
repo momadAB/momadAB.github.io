bd1e159ea9042fa04f3b10ce857d172fe6f7909a
momadAB <mabaqer99@gmail.com>
2024-08-30 14:24:32 +0300

    Replaced with CV

--- src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-function Input({ text, onChangeFunc }) {
-    return(
-        <>
-            <input type="text" value={text} onChange={onChangeFunc}/>
-        </>
-    );
-}
-
-export default Input
--- src/components/TextArea.jsx
+++
@@ -1,9 +0,0 @@
-function TextArea({ text, onChangeFunc }) {
-    return(
-        <>
-            <textarea type="text" value={text} onChange={onChangeFunc}/>
-        </>
-    );
-}
-
-export default TextArea
